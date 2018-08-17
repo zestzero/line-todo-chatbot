e386ed8f3b3300584613e15bd6853c277390feac
Krit B <Krit.b@taskworld.com>
2018-08-18 00:00:28 +0700

    :hammer: Add /callback endpoint

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
 const express = require('express');
+const app = express();
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const client = new line.Client(config);
 const app = express();
 
 app.get('/', function (req, res) {
   res.send("Hello from container land!");
 });
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
 
 const server = app.listen(process.env.PORT, function () {
   const port = server.address().port;
