066db97f429c401bdf6f34948df5dcc06483a82e
Krit B <Krit.b@taskworld.com>
2018-08-21 17:02:12 +0700

    :hammer: Add route for client request

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 require('dotenv').config()
 
 const express = require('express')
+const path = require('path')
 const Routes = require('./routes/routes')
 const MongoController = require('./services/mongo')
 
 const app = express()
 
 MongoController.init({ mongodbUri: process.env.MONGODB_URI })(function () {
+  // Serve client
+  app.use(express.static(path.join(__dirname, 'client/build')))
+
+  // Init routes
   Routes.init(app)
 })
 
