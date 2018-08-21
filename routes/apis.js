27aac4b0b16f331e10d5a91dd818230ad5615563
Krit B <Krit.b@taskworld.com>
2018-08-21 17:52:08 +0700

    :hammer: Rename payload + rename folder

--- routes/apis.js
+++ routes/apis.js
@@ -8,25 +8,25 @@ const middlewares = [ apiErrorHandler ]
 
 router.post('/task.get', middlewares, async (req, res) => {
   const schema = Joi.object().keys({
-    ownerId: Joi.string().alphanum().required()
+    owner_id: Joi.string().alphanum().required()
   })
   const { error } = Joi.validate(req.body, schema)
   if (error) return res.status(400).json(error.details)
 
-  const task = await TaskService.getTasks({ ownerId: req.body.ownerId })
+  const task = await TaskService.getTasks({ ownerId: req.body.owner_id })
   return res.json(task)
 })
 
 router.post('/task.create', middlewares, async (req, res) => {
   const schema = Joi.object().keys({
-    ownerId: Joi.string().alphanum().required(),
+    owner_id: Joi.string().alphanum().required(),
     content: Joi.string().required()
   })
   const { error } = Joi.validate(req.body, schema)
   if (error) return res.status(400).json(error.details)
 
   const task = await TaskService.createTask({
-    ownerId: req.body.ownerId,
+    ownerId: req.body.owner_id,
     content: req.body.content
   })
   return res.json(task)
@@ -34,7 +34,7 @@ router.post('/task.create', middlewares, async (req, res) => {
 
 router.post('/task.update', middlewares, async (req, res) => {
   const schema = Joi.object().keys({
-    taskId: Joi.string().alphanum().required(),
+    task_id: Joi.string().alphanum().required(),
     content: Joi.string().required(),
     order: Joi.string().required(),
     date: Joi.string().required(),
@@ -44,7 +44,7 @@ router.post('/task.update', middlewares, async (req, res) => {
   if (error) return res.status(400).json(error.details)
 
   const task = await TaskService.updateTask({
-    taskId: req.body.taskId,
+    taskId: req.body.task_id,
     content: req.body.content,
     order: req.body.order
   })
@@ -53,23 +53,23 @@ router.post('/task.update', middlewares, async (req, res) => {
 
 router.post('/task.delete', middlewares, async (req, res) => {
   const schema = Joi.object().keys({
-    taskId: Joi.string().alphanum().required()
+    task_id: Joi.string().alphanum().required()
   })
   const { error } = Joi.validate(req.body, schema)
   if (error) return res.status(400).json(error.details)
 
-  const task = await TaskService.deleteTask({ taskId: req.body.taskId })
+  const task = await TaskService.deleteTask({ taskId: req.body.task_id })
   return res.json(task)
 })
 
 router.post('/task.complete', middlewares, async (req, res) => {
   const schema = Joi.object().keys({
-    taskId: Joi.string().alphanum().required()
+    task_id: Joi.string().alphanum().required()
   })
   const { error } = Joi.validate(req.body, schema)
   if (error) return res.status(400).json(error.details)
 
-  const task = await TaskService.completeTask({ taskId: req.body.taskId })
+  const task = await TaskService.completeTask({ taskId: req.body.task_id })
   return res.json(task)
 })
 
--- services/task.js
+++ services/task.js
@@ -1,12 +1,12 @@
 const _ = require('lodash')
-const TaskDB = require('../db/task')
+const TaskModel = require('../models/task')
 const { ERROR_MSG } = require('../utils/constants')
 const { getCompactTask } = require('../utils/task')
 const { getFormatDateTime } = require('../utils/date')
 
 async function validateTask (taskId, callback) {
   try {
-    const existTask = await TaskDB.findTaskById({ taskId })
+    const existTask = await TaskModel.findTaskById({ taskId })
     if (_.isEmpty(existTask)) return { error: ERROR_MSG.TASK.NOT_FOUND }
   } catch (err) {
     return { error: err.message }
@@ -16,37 +16,37 @@ async function validateTask (taskId, callback) {
 }
 
 exports.createTask = async ({ ownerId, content, dateTime }) => {
-  const task = await TaskDB.createTask({ ownerId, content, dateTime })
+  const task = await TaskModel.createTask({ ownerId, content, dateTime })
   return getCompactTask(task)
 }
 
 exports.getTasks = async ({ ownerId }) => {
-  const tasks = await TaskDB.findTasksByOwnerId({ ownerId })
+  const tasks = await TaskModel.findTasksByOwnerId({ ownerId })
   return _.map(tasks, getCompactTask)
 }
 
 exports.getTaskById = async ({ taskId }) => {
-  const task = await TaskDB.findTaskById({ taskId })
+  const task = await TaskModel.findTaskById({ taskId })
   return getCompactTask(task)
 }
 
 exports.updateTask = async ({ taskId, content, order, date, time }) => {
   const dateTime = getFormatDateTime(date, time)
   const { result, error } = await validateTask(
-    taskId, TaskDB.updateTask({ taskId }, { content, order, date_time: dateTime })
+    taskId, TaskModel.updateTask({ taskId }, { content, order, date_time: dateTime })
   )
   if (error) return { error }
   return getCompactTask(result)
 }
 
 exports.deleteTask = async ({ taskId }) => {
-  const { result, error } = await validateTask(taskId, TaskDB.deleteTask({ taskId }))
+  const { result, error } = await validateTask(taskId, TaskModel.deleteTask({ taskId }))
   if (error) return { error }
   return getCompactTask(result)
 }
 
 exports.completeTask = async ({ taskId }) => {
-  const { result, error } = await validateTask(taskId, TaskDB.completeTask({ taskId }))
+  const { result, error } = await validateTask(taskId, TaskModel.completeTask({ taskId }))
   if (error) return { error }
   return getCompactTask(result)
 }
